236dacfc2132317e2d18a5ba15dfc7f6dac1e41f
Benson, John <jbenson@withum.com>
2017-07-18 08:50:38 -0400

    Updload Typescript Tut 1

---
+++ greeter.js
@@ -0,0 +1,14 @@
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName + " welcome to the Machine";
+}
+var user = new Student("Joe", "A.", "User");
+document.body.innerHTML = greeter(user);
